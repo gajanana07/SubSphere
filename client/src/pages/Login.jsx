6344b4aec7edd20a6ad2480af65ecb3ba3d8ae21
gajanana07 <gajnayak07@gmail.com>
2025-07-30 16:13:13 +0530

    eye button added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,6 +22,35 @@ const UserIcon = () => (
   </svg>
 );
 
+const EyeIcon = () => (
+  <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    class="bi bi-eye"
+  >
+    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+  </svg>
+);
+
+const EyeCross = () => (
+  <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    class="bi bi-eye-slash"
+  >
+    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+    <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+    <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+  </svg>
+);
+
 const EmailIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -64,6 +93,7 @@ const Login = () => {
   });
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   // this runs when the component first loads to check the user's login status.
   useEffect(() => {
@@ -77,6 +107,10 @@ const Login = () => {
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const handleChange = () => {
+    setShowPassword((item) => !item);
+  }; //for eye button
 
   const handleSubmit = async (e) => {
     //very imp!
@@ -203,19 +237,27 @@ const Login = () => {
                   <PasswordIcon />
                 </div>
                 <input
-                  name="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   placeholder=""
                   onChange={handleInputChange}
                   required
                   className="bg-slate-700 border border-slate-600 rounded-md w-full py-3 pl-10 pr-4 text-white leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
+                <div className="absolute inset-y-3 inset-x-82 right-0 pl-3 flex items-center ">
+                  <button
+                    type="button"
+                    onClick={handleChange}
+                    className="cursor-pointer"
+                  >
+                    {showPassword ? <EyeCross /> : <EyeIcon />}
+                  </button>
+                </div>
               </div>
             </div>
             <div className="mb-6">
               <button
                 type="submit"
-                className="w-full bg-blue-600 text-white font-bold py-3 px-4 rounded-md hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full bg-blue-600 text-white font-bold py-3 px-4 rounded-md hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
               >
                 {isLoginView ? "Log In" : "Create Account"}
               </button>
@@ -251,7 +293,7 @@ const Login = () => {
                 setIsLoginView(!isLoginView);
                 setError("");
               }}
-              className="font-semibold text-blue-500 hover:text-blue-400 ml-2"
+              className="font-semibold text-blue-500 hover:text-blue-400 ml-2 cursor-pointer"
             >
               {isLoginView ? "Sign Up" : "Log In"}
             </button>
